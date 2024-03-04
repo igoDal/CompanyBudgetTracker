2321c3b175a660952f046b9d2e33e5cbef56bee0
igodal <igortenn@gmail.com>
2024-03-04 23:59:26 +0100

    Added model anmmd context

---
+++ CompanyBudgetTracker/Context/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CompanyBudgetTracker.Context;
+
+public class MyDbContext : DbContext
+
+{
+    public ApplicationDbContext(DbContextOptions<MyDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<CostIncome> CostIncomes { get; set; }
+
+}
---
+++ CompanyBudgetTracker/Models/CostIncomeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyBudgetTracker.Models;
+
+public class CostIncomeModel
+{
+    public int Id { get; set; }
+    public decimal Cost { get; set; }
+    public decimal Income { get; set; }
+    public DateTime Date { get; set; }
+
+}
