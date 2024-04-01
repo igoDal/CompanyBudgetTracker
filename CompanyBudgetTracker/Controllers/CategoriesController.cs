5ef1c4a9fce576e57c899703a39b7c83ee5bc363
igodal <igortenn@gmail.com>
2024-04-01 23:28:48 +0200

    Cleaned up a bit, working on Categories.

---
+++ CompanyBudgetTracker/Controllers/CategoriesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CompanyBudgetTracker.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CompanyBudgetTracker.Context;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyBudgetTracker.Controllers;
+
+public class CategoriesController : Controller
+{
+    private readonly MyDbContext _context;
+
+    public CategoriesController(MyDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var categories = await _context.Categories.ToListAsync();
+        return View(categories);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Add(CategoryModel category)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View("Index");
+    }
+}
--- CompanyBudgetTracker/Models/CategoryModel.cs
+++ CompanyBudgetTracker/Models/CategoryModel.cs
@@ -4,7 +4,4 @@ public class CategoryModel
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public ICollection<CostIncomeModel> Transactions { get; set; }
-    public int CategoryId { get; set; }
-    public CategoryModel Category { get; set; }
 }
