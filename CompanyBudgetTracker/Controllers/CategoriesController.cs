118417becd08dccc7b38a2f951ff775aae065173
igodal <igortenn@gmail.com>
2024-04-02 23:36:02 +0200

    Still working on categories

--- CompanyBudgetTracker/Controllers/CategoriesController.cs
+++ CompanyBudgetTracker/Controllers/CategoriesController.cs
@@ -22,16 +22,109 @@ public class CategoriesController : Controller
         return View(categories);
     }
 
+    
+    public IActionResult Add()
+    {
+        return View(new CategoryModel());
+    }
+    
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Add(CategoryModel category)
     {
+        bool categoryExists = _context.Categories.Any(c => c.Name.Equals(category.Name, StringComparison.OrdinalIgnoreCase));
+        if (categoryExists)
+        {
+            ModelState.AddModelError("Name", "A category with the same name already exists.");
+            return View(category);
+        }
+
         if (ModelState.IsValid)
         {
             _context.Add(category);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-        return View("Index");
+
+        return View(category);
+    }
+    
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var category = await _context.Categories.FindAsync(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        return View(category);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, CategoryModel category)
+    {
+        if (id != category.Id)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(category);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(category.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(category);
+    }
+
+    private bool CategoryExists(int id)
+    {
+        return _context.Categories.Any(e => e.Id == id);
+    }
+    
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var category = await _context.Categories
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+        _context.Categories.Remove(category);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
     }
 }
