20417cb95804228e501651dda657a1b42072ef2d
igodal <igortenn@gmail.com>
2024-04-07 23:07:48 +0200

    Changed to Dark Mode (top bar, Home/Index for now). Changes in dashboard (date spans)

--- CompanyBudgetTracker/Controllers/HomeController.cs
+++ CompanyBudgetTracker/Controllers/HomeController.cs
@@ -33,8 +33,29 @@ public class HomeController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 
-    public async Task<IActionResult> Dashboard()
+    public async Task<IActionResult> Dashboard(DateTime? startDate, DateTime? endDate, string range = "")
     {
+        DateTime calculatedStartDate, calculatedEndDate;
+
+        switch (range.ToLower())
+        {
+            case "last30days":
+                calculatedStartDate = DateTime.Today.AddDays(-30);
+                calculatedEndDate = DateTime.Today;
+                break;
+            case "last3months":
+                calculatedStartDate = DateTime.Today.AddMonths(-3);
+                calculatedEndDate = DateTime.Today;
+                break;
+            case "lastyear":
+                calculatedStartDate = DateTime.Today.AddYears(-1);
+                calculatedEndDate = DateTime.Today;
+                break;
+            default:
+                calculatedStartDate = startDate ?? new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+                calculatedEndDate = endDate ?? DateTime.Today;
+                break;
+        }
         var viewModel = new DashboardModel
         {
             TotalIncome = await _context.CostIncomes
