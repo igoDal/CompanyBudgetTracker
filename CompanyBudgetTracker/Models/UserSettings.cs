a257947e4fe5e0d4a02a2c36686cb52d4badc766
igodal <igortenn@gmail.com>
2024-04-18 23:40:54 +0200

    Working on User settings

---
+++ CompanyBudgetTracker/Models/UserSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace CompanyBudgetTracker.Models;
+
+public class UserSettings
+{
+    public int UserSettingId { get; set; }
+    public string UserId { get; set; } 
+    public string PreferredCurrency { get; set; }
+    public DateTime FinancialYearStart { get; set; }
+    public virtual ICollection<CategoryModel> CustomCategories { get; set; }
+}
