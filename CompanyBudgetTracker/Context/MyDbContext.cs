379e51b3cad899ebf7a79598b8a90df05e83f9c3
igodal <igortenn@gmail.com>
2024-04-19 23:27:47 +0200

    Working on alerts

--- CompanyBudgetTracker/Context/MyDbContext.cs
+++ CompanyBudgetTracker/Context/MyDbContext.cs
@@ -16,11 +16,16 @@ public class MyDbContext : IdentityDbContext<IdentityUser>
     public DbSet<CostIncomeModel> CostIncomes { get; set; }
     public DbSet<AssetModel> Assets { get; set; }
     public DbSet<LiabilityModel> Liabilities { get; set; }
+    public DbSet<AlertSetting> AlertSettings { get; set; }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
+        modelBuilder.Entity<AssetModel>().HasKey(a => a.AssetId);
+        modelBuilder.Entity<LiabilityModel>().HasKey(a => a.LiabilityId);
+        
         modelBuilder.Entity<AssetModel>().ToTable("Assets");
         modelBuilder.Entity<LiabilityModel>().ToTable("Liabilities");
         modelBuilder.Entity<CostIncomeModel>().ToTable("CostIncome")
---
+++ CompanyBudgetTracker/Models/AlertSetting.cs
@@ -0,0 +1,11 @@
+﻿namespace CompanyBudgetTracker.Models;
+
+public class AlertSetting
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public decimal ThresholdAmount { get; set; }
+    public string ThresholdType { get; set; } 
+    public bool NotifyByEmail { get; set; }
+    public bool NotifyInApp { get; set; }
+}
