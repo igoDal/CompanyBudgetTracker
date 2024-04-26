1734ebc4d34dc1ed6bf8dbebaa87ce38e08e958a
igodal <igortenn@gmail.com>
2024-04-26 11:14:06 +0200

    Added controller method to save alert settings

--- CompanyBudgetTracker/Controllers/SettingsController.cs
+++ CompanyBudgetTracker/Controllers/SettingsController.cs
@@ -1,4 +1,5 @@
 ﻿using CompanyBudgetTracker.Context;
+using CompanyBudgetTracker.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -40,6 +41,33 @@ public class SettingsController : Controller
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
+        return View("Index");
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> SaveAlertSettings(AlertSetting model)
+    {
+        if (ModelState.IsValid)
+        {
+            var existingSetting = await _context.AlertSettings.FindAsync(model.Id);
+            if (existingSetting != null)
+            {
+                
+                existingSetting.ThresholdType = model.ThresholdType;
+                existingSetting.ThresholdAmount = model.ThresholdAmount;
+                existingSetting.NotifyByEmail = model.NotifyByEmail;
+                existingSetting.NotifyInApp = model.NotifyInApp;
+            }
+            else
+            {
+                _context.AlertSettings.Add(model);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
         return View("Index");
     }
 }
