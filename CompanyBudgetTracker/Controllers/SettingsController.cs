ee3ec7dd36622e582b41da140a477c53bcd232d0
igodal <igortenn@gmail.com>
2024-05-05 18:59:35 +0200

    Working on user settings save to db

--- CompanyBudgetTracker/Controllers/SettingsController.cs
+++ CompanyBudgetTracker/Controllers/SettingsController.cs
@@ -40,40 +40,66 @@ public class SettingsController : Controller
         return View(userSetting);
     }
 
+    
+    public async Task<IActionResult> EditUserSettings()
+    {
+        var userId = _userManager.GetUserId(User);
+        var userSetting = await _context.UserSettings.FirstOrDefaultAsync(us => us.UserId == userId);
 
+        if (userSetting == null)
+        {
+            userSetting = new UserSettings
+            {
+                UserId = userId,
+                EnableNotifications = true,
+                NotifyByEmail = true,
+                NotifyBySMS = false,
+                NotifyInApp = true,
+                NotifyOnNewMessage = true,
+                NotifyOnTaskCompletion = true,
+                NotifyOnDueDateApproach = true,
+                Language = "en-US",
+                Theme = "Light"
+            };
+        }
 
+        
+        return View(userSetting);
+    }
 
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(UserSettings userSetting)
+    public async Task<IActionResult> EditUserSettings(UserSettings model)
     {
-        if (ModelState.IsValid)
+        /*if (!ModelState.IsValid)
         {
-            var dbEntry = await _context.UserSettings.FindAsync(userSetting.Id);
-            if (dbEntry != null)
-            {
-                dbEntry.EnableNotifications = userSetting.EnableNotifications;
-                dbEntry.Theme = userSetting.Theme; 
-                
-                dbEntry.NotifyByEmail = userSetting.NotifyByEmail;
-                dbEntry.NotifyBySMS = userSetting.NotifyBySMS;
-                dbEntry.NotifyInApp = userSetting.NotifyInApp;
-                dbEntry.NotifyOnNewMessage = userSetting.NotifyOnNewMessage;
-                dbEntry.NotifyOnTaskCompletion = userSetting.NotifyOnTaskCompletion;
-                dbEntry.NotifyOnDueDateApproach = userSetting.NotifyOnDueDateApproach;
-                
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            else
-            {
-                _context.UserSettings.Add(userSetting);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
+            return View(model);
+        }*/
+
+        var userSetting = await _context.UserSettings.FindAsync(model.Id);
+        if (userSetting != null)
+        {
+            userSetting.EnableNotifications = model.EnableNotifications;
+            userSetting.NotifyByEmail = model.NotifyByEmail;
+            userSetting.NotifyBySMS = model.NotifyBySMS;
+            userSetting.NotifyInApp = model.NotifyInApp;
+            userSetting.NotifyOnNewMessage = model.NotifyOnNewMessage;
+            userSetting.NotifyOnTaskCompletion = model.NotifyOnTaskCompletion;
+            userSetting.NotifyOnDueDateApproach = model.NotifyOnDueDateApproach;
+            userSetting.Language = model.Language;
+            userSetting.Theme = model.Theme;
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
-        return View("EditUserSettings");
+        else
+        {
+            _context.UserSettings.Add(model);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
     }
+
     
     [HttpPost]
     [ValidateAntiForgeryToken]
