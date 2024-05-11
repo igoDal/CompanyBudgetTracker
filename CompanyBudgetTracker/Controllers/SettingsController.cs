2e01f7b76b6ee1339a62952562c9c449ae3a3961
igodal <igortenn@gmail.com>
2024-05-11 13:36:45 +0200

    Updated SettingsController to use CurrenTuserService. Refactored SettingsController

--- CompanyBudgetTracker/Controllers/SettingsController.cs
+++ CompanyBudgetTracker/Controllers/SettingsController.cs
@@ -1,4 +1,5 @@
 ﻿using CompanyBudgetTracker.Context;
+using CompanyBudgetTracker.Interfaces;
 using CompanyBudgetTracker.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -7,65 +8,25 @@ using Microsoft.EntityFrameworkCore;
 public class SettingsController : Controller
 {
     private readonly MyDbContext _context;
-    private readonly UserManager<IdentityUser> _userManager;
+    private readonly ICurrentUserService _currentUserService;
 
-    public SettingsController(MyDbContext context, UserManager<IdentityUser> userManager)
+    public SettingsController(MyDbContext context, ICurrentUserService currentUserService)
     {
         _context = context;
-        _userManager = userManager;
+        _currentUserService = currentUserService;
     }
 
     public async Task<IActionResult> Index()
     {
-        var userId = _userManager.GetUserId(User);
-        var userSetting = await _context.UserSettings.FirstOrDefaultAsync(us => 
-            us.UserId == userId) ?? GetDefaultUserSettings(userId);
-
-        if (userSetting == null)
-        {
-            userSetting = new UserSettings
-            {
-                UserId = userId,
-                EnableNotifications = true,
-                NotifyByEmail = true,
-                NotifyBySMS = false,
-                NotifyInApp = true,
-                NotifyOnNewMessage = true,
-                NotifyOnTaskCompletion = true,
-                NotifyOnDueDateApproach = true,
-                Language = "en-US",
-                Theme = "Light"
-            };
-        }
-
+        var userSetting = await GetUserSettings();
+        
         return View(userSetting);
     }
 
     
     public async Task<IActionResult> EditUserSettings()
     {
-        var userId = _userManager.GetUserId(User);
-        var userSetting = await _context.UserSettings.FirstOrDefaultAsync(us => 
-            us.UserId == userId) ?? GetDefaultUserSettings(userId);
-
-        if (userSetting == null)
-        {
-            userSetting = new UserSettings
-            {
-                UserId = userId,
-                EnableNotifications = true,
-                NotifyByEmail = true,
-                NotifyBySMS = false,
-                NotifyInApp = true,
-                NotifyOnNewMessage = true,
-                NotifyOnTaskCompletion = true,
-                NotifyOnDueDateApproach = true,
-                Language = "en-US",
-                Theme = "Light"
-            };
-        }
-
-        
+        var userSetting = await GetUserSettings();
         return View(userSetting);
     }
 
@@ -73,33 +34,22 @@ public class SettingsController : Controller
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> EditUserSettings(UserSettings model)
     {
-        /*if (!ModelState.IsValid)
+        if (!ModelState.IsValid)
         {
             return View(model);
-        }*/
+        }
 
         var userSetting = await _context.UserSettings.FindAsync(model.Id);
         if (userSetting != null)
         {
-            userSetting.EnableNotifications = model.EnableNotifications;
-            userSetting.NotifyByEmail = model.NotifyByEmail;
-            userSetting.NotifyBySMS = model.NotifyBySMS;
-            userSetting.NotifyInApp = model.NotifyInApp;
-            userSetting.NotifyOnNewMessage = model.NotifyOnNewMessage;
-            userSetting.NotifyOnTaskCompletion = model.NotifyOnTaskCompletion;
-            userSetting.NotifyOnDueDateApproach = model.NotifyOnDueDateApproach;
-            userSetting.Language = model.Language;
-            userSetting.Theme = model.Theme;
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
+            _context.Entry(userSetting).CurrentValues.SetValues(model);
         }
         else
         {
             _context.UserSettings.Add(model);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
         }
+        await _context.SaveChangesAsync();
+        return RedirectToAction("Index");
     }
 
     
@@ -112,11 +62,7 @@ public class SettingsController : Controller
             var existingSetting = await _context.AlertSettings.FindAsync(model.Id);
             if (existingSetting != null)
             {
-                
-                existingSetting.ThresholdType = model.ThresholdType;
-                existingSetting.ThresholdAmount = model.ThresholdAmount;
-                existingSetting.NotifyByEmail = model.NotifyByEmail;
-                existingSetting.NotifyInApp = model.NotifyInApp;
+                _context.Entry(existingSetting).CurrentValues.SetValues(model);
             }
             else
             {
@@ -129,7 +75,20 @@ public class SettingsController : Controller
 
         return View("Index");
     }
-    
+
+    private async Task<UserSettings> GetUserSettings()
+    {
+        var userId = _currentUserService.GetUserId();
+        var userSetting = await _context.UserSettings.FirstOrDefaultAsync(us => us.UserId == userId);
+        if (userSetting == null)
+        {
+            userSetting = GetDefaultUserSettings(userId);
+            _context.UserSettings.Add(userSetting);
+            await _context.SaveChangesAsync();
+        }
+
+        return userSetting;
+    }
     private UserSettings GetDefaultUserSettings(string userId)
     {
         return new UserSettings
